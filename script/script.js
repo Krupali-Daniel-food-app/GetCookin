48a38bbebd636c3cf0ee71d363dffb7c4b483dae
Krupali Thakkar <krupalitp@outlook.com>
2020-07-28 13:07:00 -0400

    adding files and setups

---
+++ script/script.js
@@ -0,0 +1,27 @@
+// We are going to build an app that will retrive recipes from api
+
+​//namespace
+
+// Recipe API key and the URL
+
+​// Initialize the init
+
+// Setup first page at initial load
+​
+// Watch for user input in search bar 
+
+// Make sure users select atleast 3 ingredients (3 search bars)
+
+//retrive data from the api to reflect the recipes on page
+
+// get multiple recipes
+
+//display recipe title, image and link to the recipe
+​
+// Function to reset page on refresh
+
+// Doc Ready
+
+// stretch goals -
+
+//display time to cook recipe and nutritional count on DOM for users
